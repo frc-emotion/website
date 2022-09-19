dac92997fbe59d93f6943d2adf28af57c31a0d50
Jason Ballinger <jasonballinger_dev@yahoo.com>
2022-09-18 19:32:33 -0700

    import project outline

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div></div>
+  )
+}
