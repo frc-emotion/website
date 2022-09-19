b74055c82d7f4cad208c5da19319cffcca43b6df
Jason Ballinger <jasonballinger_dev@yahoo.com>
2022-09-18 21:34:02 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import '../styles/globals.css'
-import Layout from '../components/layout'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
       <Component {...pageProps} />
-    </Layout>
   )
 }
