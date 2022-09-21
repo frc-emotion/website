2bed42dcade7e39f9a4eb259391d3d3322fd658b
Jason Ballinger <jasonballinger_dev@yahoo.com>
2022-09-21 15:06:22 -0700

    add contact form

---
+++ components/ContactForm.js
@@ -0,0 +1,34 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+export default function ContactForm() {
+  return (
+    <Form name="contact" action="/success" method="POST" data-netlify="true">
+      <Form.Control type="hidden" name="contact-form" value="ContactForm" />
+
+      <Form.Group className="mb-3">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="text" name="name" id="name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Company</Form.Label>
+        <Form.Control type="text" name="company" id="company" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" name="email" id="email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Message</Form.Label>
+        <Form.Control type="textarea" name="message" id="message" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
---
+++ components/ContactInfo.js
@@ -0,0 +1,8 @@
+
+export default function ContactInfo() {
+  return (
+    <div>
+        
+    </div>
+  );
+}
