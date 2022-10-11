1842c4eab4067fb11085d7c2038fd4c74fd4afdc
Jason Ballinger <jasonballinger_dev@yahoo.com>
2022-10-10 18:48:45 -0700

    push - publish website

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from "next/head";
+import Image from "next/image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import PageHeader from "../components/PageHeader";
+
+import rowStyles from "../styles/rows.module.css";
 
 export default function Home() {
   return (
@@ -11,7 +17,23 @@ export default function Home() {
           content="Team Σ-Motion is a high school FIRST robotics team at Rancho Bernardo High School dedicated to promoting Science, Technology, Engineering, and Mathematics."
         />
       </Head>
-      <PageHeader title="Home" />
+      <PageHeader title="Home" image="url(header-img/home.jpg)" />
+      <Container fluid>
+        <Row className={rowStyles.secondaryRow}>
+          <Col>
+            <h2>Geared for the Future</h2>
+            <p>
+              We are a FRC Robotics team founded in 2008, by and for high school
+              students. We are dedicated to giving students an experience that
+              will help them in our ever-evolving world. We are a 100%
+              student-run team, so students get the amazing opportunity to learn
+              from their older peers and teammates. We show students what it&apos;s
+              like to work on a project as a team, and how fun it is when the
+              robot you built is the one that wins.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- pages/sponsors.js
+++ pages/sponsors.js
@@ -1,9 +1,19 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import PageHeader from "../components/PageHeader";
 
 export default function Sponsors() {
   return (
     <>
-      <PageHeader title="Sponsors" />
+      <PageHeader title="Sponsors" image="url(header-img/sponsors.jpg)" />
+      <Container>
+        <Row>
+          <Col>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
