a215d564a59395a415309f6fa82dd568756edc4a
Jason Ballinger <jasonballinger_dev@yahoo.com>
2022-10-21 13:44:31 -0700

    update environment variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    WEBHOOK_URL:
+      "https://discord.com/api/webhooks/1029203236269342741/ugpwRWCMQ06Iz5x0nosTHnSTYIAL5X3J0SCTsbWQrO0JLttX87p5TjMdb8ltwsPypF1j",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
