c859d35d1988145f921deaf2993395e2c84b5164
Jason Ballinger <jasonballinger_dev@yahoo.com>
2022-10-21 23:32:42 -0700

    setup site for static exporting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withOptimizedImages = require("next-optimized-images");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+(module.exports = nextConfig),
+  withOptimizedImages({
+    handleImages: ["jpeg", "png", "svg", "webp", "gif"],
+  });
