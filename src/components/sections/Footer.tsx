21b7a6826779a1b70729d6a9318c47336c7b9bff
Jason Ballinger <jasonballinger_dev@yahoo.com>
2023-05-31 16:46:49 -0700

    update footer bg

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -6,7 +6,6 @@ export default function Footer() {
 	return (
 		<>
 			<div className="h-[40vh] grid bg-black text-teamYellow pl-[10em] pr-[10em]"></div>
-			<div className="h-[2vh] bg-teamYellow" />
 		</>
 	);
 }
