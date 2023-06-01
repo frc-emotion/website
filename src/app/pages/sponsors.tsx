09e134c731da39fbdd3157522be6ebe2c0998e5d
Peyton S <64993348+PSlape2@users.noreply.github.com>
2023-05-31 23:37:23 -0700

    Added 2023 sponsors box

---
+++ src/app/pages/sponsors.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navigation/Navbar";
+import Footer from "@/components/sections/Footer";
+import SponsorImages2023 from "@/components/sections/sponsors/SponsorImages2023";
+
+export default function Sponsor() {
+    return (
+        <main>
+            <Navbar />
+            <SponsorImages />
+            <Footer />
+        </main>
+    );
+} 
---
+++ src/components/sections/sponsors/SponsorImages2023.tsx
@@ -0,0 +1,23 @@
+import '@/styles/globals.css';
+
+export default function SponsorImages2023() {
+    return (
+        <div>
+            <div className="text-2xl text-center">
+                <h1>Our 2023 Sponsors</h1>
+            </div>
+            <div className="grid grid-cols-3 gap-10">
+                
+                <img src="https://www.defense.gov/portals/1/Page-Assets/branding-guide/logos/png/DoD-Logo.png" width="500" height="500"></img>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Qualcomm-Logo.svg/512px-Qualcomm-Logo.svg.png?20191123135426" width="500" height="500"></img>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Northrop_Grumman_logo_blue-on-clear_2020.svg/800px-Northrop_Grumman_logo_blue-on-clear_2020.svg.png?20200301125825" width="500" height="500"></img>
+                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/92/General_Atomics_Logo.svg/220px-General_Atomics_Logo.svg.png" width="500" height="500"></img>
+                <img src="https://cdn-gcppc.nitrocdn.com/ptXvfPuULgBMoaHTEkAhxPIvvnllWqnh/assets/images/optimized/rev-365445b/wp-content/uploads/2021/02/cropped-batteries-plus-logo-220x68.png" width="500" height="500"></img>
+                
+                <center>
+                    <img src="https://scontent-lax3-1.xx.fbcdn.net/v/t39.30808-6/307455399_427026382908073_4508787573902862313_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=pOh9jR5s0-sAX-Q8Pts&_nc_ht=scontent-lax3-1.xx&oh=00_AfDmdXz3MnApVgZdbIFQGrX7xe4c32ALaUt9rGcKB9wjZw&oe=647D8B75" width="150" height="100"></img>
+                </center>
+            </div>
+        </div>
+    );
+} // 
