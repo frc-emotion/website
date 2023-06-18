d8770eca228edf38fb69ef0a44efb8beb46a4ad1
Nova Mondal <rialmondal@gmail.com>
2023-06-18 16:36:46 -0700

    Removed About Us from homepage, moved into own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,22 +23,10 @@ export default function Home() {
 					<div className="col-span-1 lg:mx-[4vw] mx-[10vw]">
 						<div className="flex-col">
 							<h1 className="text-[50px] font-semibold">
-								About Us
+								Team Info
 							</h1>
 							<p className="text-[17px]">
-								We are an FRC Robotics team founded in 2008, by
-								and for high school students. We are dedicated
-								to giving students an experience that will help
-								them in our ever-evolving world. We are a 100%
-								student-run team, so students get the amazing
-								opportunity to learn from their older peers and
-								teammates. We show students what it's like to
-								work on a project as a team, and how fun it is
-								when the robot you built is the one that wins.
-								Since its establishment in 2007 by Jeff Bush and
-								Richard Lin, the Σ-Motion team has designed and
-								built over 10 robots which it used in the FIRST
-								Competitions around the country.
+								FRC Team 2658, Σ-Motion, is the robotics team of Rancho Bernardo High School. We are located at 13010 Paseo Lucido, San Diego, CA 92128. 
 							</p>
 							<h2 className="text-[40px] font-semibold mt-10">
 								Mission Statement
---
+++ src/components/sections/about/AboutUs.tsx
@@ -0,0 +1,19 @@
+export default function AboutUs(){
+    return (
+		<div className="flex-col flex">
+			<h1 className="text-[50px] font-semibold">About Us</h1>
+			<p className="text-[17px]">
+				We are an FRC Robotics team founded in 2008, by and for high
+				school students. We are dedicated to giving students an
+				experience that will help them in our ever-evolving world. We
+				are a 100% student-run team, so students get the amazing
+				opportunity to learn from their older peers and teammates. We
+				show students what it's like to work on a project as a team, and
+				how fun it is when the robot you built is the one that wins.
+				Since its establishment in 2007 by Jeff Bush and Richard Lin,
+				the Σ-Motion team has designed and built over 10 robots which it
+				used in the FIRST Competitions around the country.
+			</p>
+		</div>
+	);
+}
