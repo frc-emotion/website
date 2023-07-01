5828deb379f70884757ebec1954fdaabc80d43b9
Nova Mondal <rialmondal@gmail.com>
2023-06-30 22:43:21 -0700

    refactoring

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -1,13 +1,9 @@
-import Navbar from "@/components/navigation/Navbar";
-import Footer from "@/components/sections/Footer";
 import Hero from "@/components/sections/contact/Hero";
 
 export default function Home() {
 	return (
 		<main>
-			<Navbar />
 			<Hero />
-			<Footer />
 		</main>
 	);
 }
