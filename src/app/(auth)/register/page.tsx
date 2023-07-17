010f4cc01539964851b62afe2283844348851552
Jason Ballinger <jasonballinger_dev@yahoo.com>
2023-07-16 21:46:00 -0700

    fix order of create account form elements

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -63,43 +63,47 @@ async function createAccount(
 	FirstName: string,
 	LastName: string,
 	Email: string,
-    router: AppRouterInstance
+	router: AppRouterInstance
 ) {
-		try {
-			const request: RequestInit = {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					username: Username,
-					password: Password,
-					firstname: FirstName,
-					lastname: LastName,
-					email: Email,
-				}),
-				redirect: "follow",
-			};
-			const response = await fetch(
-				"https://api.team2658.org/v1/users/register",
-				request
+	try {
+		const request: RequestInit = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				username: Username,
+				password: Password,
+				firstname: FirstName,
+				lastname: LastName,
+				email: Email,
+			}),
+			redirect: "follow",
+		};
+		const response = await fetch(
+			"https://api.team2658.org/v1/users/register",
+			request
+		);
+		const txt = await response.text();
+		if (response.status === 201) {
+			alert(
+				"Account created successfully!\nPlease see a lead to get your account verified."
 			);
-			const txt = await response.text();
-			if (response.status === 201) {
-				alert("Account created successfully!\nPlease see a lead to get your account verified.");
-                router.push('/login');
-			} else {
-				alert(
-					`Error ${response.status}: ${
-						response.statusText
-					}\n${txt.slice(12, txt.length - 2)/* Error message from server */}`
-				);
-			}
-		} catch (e) {
-			console.error(`COULD NOT CREATE ACCOUNT: ERROR ${e}`);
+			router.push("/login");
+		} else {
+			alert(
+				`Error ${response.status}: ${response.statusText}\n${
+					txt.slice(
+						12,
+						txt.length - 2
+					) /* Error message from server */
+				}`
+			);
 		}
+	} catch (e) {
+		console.error(`COULD NOT CREATE ACCOUNT: ERROR ${e}`);
 	}
-
+}
 
 export default function Register() {
 	const [username, setUsername] = useState("");
@@ -129,19 +133,81 @@ export default function Register() {
 						<form
 							className="mt-3"
 							onSubmit={(e) => {
-								if(password2!==password) return;
-                                e.preventDefault();
-                                createAccount(
-                                    username,
-                                    password,
-                                    firstName,
-                                    lastName,
-                                    email,
-                                    router
-                                );
+								if (password2 !== password) return;
+								e.preventDefault();
+								createAccount(
+									username,
+									password,
+									firstName,
+									lastName,
+									email,
+									router
+								);
 							}}
 						>
 							<div className="grid grid-cols-1">
+								<div className="grid grid-cols-2 space-x-5 w-fit">
+									<div className="p-1.5">
+										<label
+											htmlFor="firstName"
+											className="text-gray-700"
+										>
+											First Name
+										</label>
+										<input
+											required
+											type="text"
+											id="firstName"
+											name="firstName"
+											className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+											placeholder=""
+											value={firstName}
+											onChange={(e) => {
+												setFirstName(e.target.value);
+											}}
+										/>
+									</div>
+									<div className="p-1.5">
+										<label
+											htmlFor="firstName"
+											className="text-gray-700"
+										>
+											Last Name
+										</label>
+										<input
+											required
+											type="text"
+											id="firstName"
+											name="firstName"
+											className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+											placeholder=""
+											value={lastName}
+											onChange={(e) => {
+												setLastName(e.target.value);
+											}}
+										/>
+									</div>
+								</div>
+								<div className="block p-1.5">
+									<label
+										htmlFor="email"
+										className="text-gray-700"
+									>
+										Email
+									</label>
+									<input
+										required
+										type="email"
+										id="email"
+										name="email"
+										className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+										placeholder=""
+										value={email}
+										onChange={(e) => {
+											setEmail(e.target.value);
+										}}
+									/>
+								</div>
 								<div className="block p-1.5">
 									<label
 										htmlFor="usernameInput"
@@ -150,7 +216,7 @@ export default function Register() {
 										Username
 									</label>
 									<input
-                                        required
+										required
 										type="text"
 										id="usernameInput"
 										name="usernameInput"
@@ -170,7 +236,7 @@ export default function Register() {
 										Password
 									</label>
 									<input
-                                        required
+										required
 										type="password"
 										id="passwordInput"
 										name="passwordInput"
@@ -187,10 +253,20 @@ export default function Register() {
 										htmlFor="passwordInputRepeat"
 										className="text-gray-700"
 									>
-										Repeat Password <span className={(password&&(password!=password2))? "text-red-400 text-sm" : "hidden"}>Passwords don't match</span>
+										Repeat Password{" "}
+										<span
+											className={
+												password &&
+												password != password2
+													? "text-red-400 text-sm"
+													: "hidden"
+											}
+										>
+											Passwords don't match
+										</span>
 									</label>
 									<input
-                                        required
+										required
 										type="password"
 										id="passwordInputRepeat"
 										name="passwordInputRepeat"
@@ -199,66 +275,6 @@ export default function Register() {
 										value={password2}
 										onChange={(e) => {
 											setPassword2(e.target.value);
-										}}
-									/>
-								</div>
-								<div className="block p-1.5">
-									<label
-										htmlFor="firstName"
-										className="text-gray-700"
-									>
-										First Name
-									</label>
-									<input
-                                        required
-                                        type="text"
-										id="firstName"
-										name="firstName"
-										className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-										placeholder=""
-										value={firstName}
-										onChange={(e) => {
-											setFirstName(e.target.value);
-										}}
-									/>
-								</div>
-								<div className="block p-1.5">
-									<label
-										htmlFor="firstName"
-										className="text-gray-700"
-									>
-										Last Name
-									</label>
-									<input
-                                        required
-                                        type="text"
-										id="firstName"
-										name="firstName"
-										className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-										placeholder=""
-										value={lastName}
-										onChange={(e) => {
-											setLastName(e.target.value);
-										}}
-									/>
-								</div>
-								<div className="block p-1.5">
-									<label
-										htmlFor="email"
-										className="text-gray-700"
-									>
-										Email
-									</label>
-									<input
-                                        required
-                                        type="email"
-										id="email"
-										name="email"
-										className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-										placeholder=""
-										value={email}
-										onChange={(e) => {
-											setEmail(e.target.value);
 										}}
 									/>
 								</div>
