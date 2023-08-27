4b9daf205b4e02a35e97595a421cc608ddadc5ca
Jason Ballinger <jasonballinger_dev@yahoo.com>
2023-08-27 12:08:51 -0700

    fixed font changing issues?

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,21 +2,35 @@ import Navbar from "@/components/navigation/Navbar";
 import Footer from "@/components/sections/Footer";
 import { cookies } from "next/dist/client/components/headers";
 import { User } from "../(auth)/login/page";
+import { Bebas_Neue, Orbitron } from "next/font/google";
+
+const bbnFont = Bebas_Neue({
+	subsets: ["latin"],
+	weight: "400",
+	display: "swap",
+	variable: "--font-bebas-neue",
+});
+
+const orbFont = Orbitron({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-main",
+});
 
 export default function PublicLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-    const cookieJar = cookies();
-    const auth = cookieJar.get("auth");
-    const userCookie = cookieJar.get("user")?.value;
-    const user = userCookie ? (JSON.parse(userCookie) as User) : null;
-    return (
-        <>
-            <Navbar auth={auth?.value === "true"} user={user} />
-            {children}
-            <Footer />
-        </>
-    );
+	const cookieJar = cookies();
+	const auth = cookieJar.get("auth");
+	const userCookie = cookieJar.get("user")?.value;
+	const user = userCookie ? (JSON.parse(userCookie) as User) : null;
+	return (
+		<div id="publicLayout" className={`${bbnFont.variable} ${orbFont.variable} font-sans bg-black`}>
+			<Navbar auth={auth?.value === "true"} user={user} />
+			{children}
+			<Footer />
+		</div>
+	);
 }
