92bc791ccd2f9d61df3b8f7b9b21905046a603c7
Jason Ballinger <jason.ballinger06@gmail.com>
2023-11-13 11:37:35 -0800

    login/register functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,27 +2,26 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 
 export const metadata = {
-	title: "FRC Team 2658",
-	description:
-		"Team Σ-Motion is a high school FIRST robotics team at Rancho Bernardo High School dedicated to promoting Science, Technology, Engineering, and Mathematics.",
+    title: "FRC Team 2658",
+    description:
+        "Team Σ-Motion is a high school FIRST robotics team at Rancho Bernardo High School dedicated to promoting Science, Technology, Engineering, and Mathematics.",
 };
 
 const font = Inter({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-main",
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-main",
 });
 
 export default function RootLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={`${font.variable} font-sans bg-black`}>
-				{children}
-			</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            {/* <body className={`${font.variable} font-sans bg-black`}> */}
+            <body>{children}</body>
+        </html>
+    );
 }
