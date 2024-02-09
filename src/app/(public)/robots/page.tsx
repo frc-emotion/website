8aa88343ac86a7a7a5d69f5f427e27dde0e3ed53
Nathan Gillespie <crookedtwig4@gmail.com>
2024-02-08 17:32:21 -0800

    Stay Fruity

---
+++ src/app/(public)/robots/page.tsx
@@ -0,0 +1,30 @@
+import Navbar from "@/components/navigation/Navbar";
+import Footer from "@/components/sections/Footer";
+import Hero from "@/components/sections/home/Hero";
+import RobotPanel from "@/components/sections/robots/robot";
+import Image from "next/image";
+
+export const metadata = {
+	title: "Robots | FRC Team 2658",
+	description:
+		"Sponsorship information for FRC Team 2658, Σ-Motion, from Rancho Bernardo, California",
+};
+
+export default function Robot() {
+	return (
+		<main className="bg-black">
+			<div className="relative h-clampLarge overflow-hidden">
+				<Image
+					src="https://cdn.team2658.org/web-public/trident.jpeg"
+					alt="2023 Robot, Trident"
+					fill={true}
+					className="absolute object-cover blur-sm brightness-50"
+				/>
+				<h1 className="absolute inset-0 py-8 text-center text-r8xl font-bold text-teamYellow-100">
+					Robots
+				</h1>
+			</div>
+			<RobotPanel/>
+		</main>
+	);
+}
