66f4f19f5624c092a8f3156a7ab085125b84cca7
ArshansGithub <arshan@arshan.dev>
2024-05-11 17:11:36 -0700

    scrolling pdf with some styling

--- src/app/(public)/donate/page.tsx
+++ src/app/(public)/donate/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PDFViewer from '@/components/sections/donate/PDFViewer';
-
+import Image from "next/image";
 
 export const metadata = {
     title: "Donate | FRC Team 2658",
@@ -10,13 +10,19 @@ export const metadata = {
 export default function Home ({ children }: { children: React.ReactNode }) {
     return (
         <div>
-        <h1 className="text-3xl font-bold">Donate</h1>
-        <p>Thank you for considering a donation!</p>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Donate Now
-        </button>
-        <div className="mt-8">
-            <PDFViewer>{children}</PDFViewer>
+        <div className="relative overflow-hidden h-clampLarge mb-8  ">
+                <Image
+                    src="https://cdn.team2658.org/web-public/trident.jpeg"
+                    alt="2023 Robot, Trident"
+                    fill={true}
+                    className="object-cover absolute brightness-50 blur-sm"
+                />
+                <h1 className="text-center py-8 text-r8xl font-bold absolute inset-0 text-teamYellow-100">
+                    Donate
+                </h1>
+            </div>
+        <div className="flex justify-center w-1/2 flex-col mx-auto bg-teamYellow-500 rounded-xl">
+            <PDFViewer/>
         </div>
     </div>
     );
--- src/components/sections/donate/PDFViewer.tsx
+++ src/components/sections/donate/PDFViewer.tsx
@@ -1,22 +1,54 @@
-'use client';
+"use client"
+
 import { Document, Page, pdfjs } from 'react-pdf';
+import { useState } from 'react';
+import 'react-pdf/dist/esm/Page/TextLayer.css'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.js',
-    import.meta.url,
-  ).toString();
- 
-export default function PDFViewer({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
+export default function PDFViewer() {
+  const [numPages, setNumPages] = useState<number>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
   return (
     <>
-      <Document file="Sponsor.pdf">
-                <Page pageNumber={1} />
-            </Document>
-      {children}
+        <div className="m-2 flex flex-row justify-around items-center">
+          
+          <button className="bg-red-800 ml-16 outline-dashed outline-2 outline-offset-2"></button>
+        </div>
+
+        {/* <Document className="" file="Sponsor.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document> */}
+      <div className="flex flex-col justify-around items-center bg-gray-600">
+
+      <Document
+      className="flex flex-row justify-around items-center mb-8"
+      file="Sponsor.pdf"
+      onLoadSuccess={onDocumentLoadSuccess}
+    >
+      <div className="relative overflow-y-auto overflow-x-hidden max-h-[720px] rounded-xl">
+        {numPages &&
+          Array.from({ length: numPages }, (_, index) => index + 1).map(
+            (pageNumber) => <Page pageNumber={pageNumber} />
+          )}
+      </div>
+      </Document>
+
+          <div className='absolute h-16 bg-red-500'>
+            <h1>hsadhajs</h1> 
+          </div>
+    </div>
+    
+    
     </>
   )
 }
