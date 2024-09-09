2b19a9b7c890eca1999a3e4bcff29e04548bde2d
Arshan <111618520+ArshansGithub@users.noreply.github.com>
2024-09-08 17:18:29 -0700

    missing?

--- next.config.js
+++ next.config.js
@@ -12,6 +12,13 @@ const nextConfig = {
 	sassOptions: {
 		includePaths: [require("path").join(__dirname, "styles")],
 	},
+	experimental: {
+		turbo: {
+			resolveAlias: {
+				canvas: './empty-module.ts'
+			}
+		}
+	}
 };
 
 module.exports = nextConfig;
