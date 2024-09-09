a2aca69c3e74bfd06fc6fd998dc75118acc051d5
Arshan <111618520+ArshansGithub@users.noreply.github.com>
2024-09-08 17:25:04 -0700

    turbo causing problems

--- src/components/sections/donate/PDFViewer.tsx
+++ src/components/sections/donate/PDFViewer.tsx
@@ -6,6 +6,19 @@ import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import { useEffect, useState } from "react";
 
+if (typeof Promise.withResolvers === 'undefined') {
+  if (window)
+      // @ts-expect-error This does not exist outside of polyfill which this is doing
+      window.Promise.withResolvers = function () {
+          let resolve, reject;
+          const promise = new Promise((res, rej) => {
+              resolve = res;
+              reject = rej;
+          });
+          return { promise, resolve, reject };
+      };
+}
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 let filename = "Sponsorship Packet 2024-25.pdf";
