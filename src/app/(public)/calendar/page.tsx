11a752badcacdfa5d72868b9a4c9c954d3fb58a2
Nathan Gillespie <crookedtwig4@gmail.com>
2024-10-09 13:21:09 -0700

    Make Calendar page look better

--- src/app/(public)/calendar/page.tsx
+++ src/app/(public)/calendar/page.tsx
@@ -1,29 +1,31 @@
 import { global } from "styled-jsx/css"
-
+import Image from "next/image";
 
 export default function Calendar() {
     return(
-        <main className="bg-teamYellow-500 overflow-hidden relative">
+        <main className="overflow-hidden relative">
             <div className="relative inset-x-0 top-0 h-clampLarge w-full overflow-hidden object-cover">
-                <h1 className="absolute inset-0 my-8 text-center text-r8xl font-bold">
+				{/* Set to new image */}
+                <Image
+                    src="https://cdn.team2658.org/web-public/lvr-stands.jpg"
+                    alt="Σ-Motion at the Las Vegas Regional"
+                    fill={true}
+                    className="absolute overflow-hidden object-cover blur-sm brightness-50"
+                />
+                <h1 className="absolute inset-0 my-8 text-center text-r8xl font-bold text-teamYellow-100">
                         Calendar
                 </h1>
             </div>
             <div className="bg-teamYellow-500 flex flex-col items-center content-center">
-                <div className="bg-black w-full">
-                    <h1 className="font-bold my-10 text-teamYellow-400 text-center text-2xl">Public Calendar</h1>
-                </div>
-
                 {/* REPLACE WITH ACTUAL TEAM CALENDAR */}
                 <iframe 
-                    className="py-5"
+                    className="p-5 m-10 rounded-lg bg-white"
                     src="https://calendar.google.com/calendar/embed?src=c_dee5e860860da8c03dbc378d500f8e87b1c31c35599e2b7396f7f68108746a2b%40group.calendar.google.com&ctz=America%2FLos_Angeles" 
                     width="50%" 
                     height="600">
                 </iframe>
-                
             </div>
-            
+
         </main>
     )
 }
